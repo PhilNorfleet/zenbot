a7ff5bd6c48546da6835ac1abca28d1107f5475f
PhilNorfleet <norfleet.phillip@gmail.com>
2017-05-25 18:59:05 -0400

    resets sample conf to default values

--- conf-sample.js
+++ conf-sample.js
@@ -29,15 +29,15 @@ c.poloniex.secret = 'YOUR-SECRET'
 // Optional stop-order triggers:
 
 // sell if price drops below this % of bought price (0 to disable)
-c.sell_stop_pct = 8
+c.sell_stop_pct = 0
 // buy if price surges above this % of sold price (0 to disable)
-c.buy_stop_pct = 4
+c.buy_stop_pct = 0
 // sell if price rises this % in a period
-c.sell_pop_pct = 4
+c.sell_pop_pct = 0
 // buy if price drops this % in a period
-c.buy_drop_pct = 4
+c.buy_drop_pct = 0
 // enable trailing sell stop when reaching this % profit (0 to disable)
-c.profit_stop_enable_pct = 8
+c.profit_stop_enable_pct = 0
 // maintain a trailing stop this % below the high-water mark of profit
 c.profit_stop_pct = 2
 
@@ -46,9 +46,9 @@ c.profit_stop_pct = 2
 // avoid trading at a slippage above this pct
 c.max_slippage_pct = 5
 // buy with this % of currency balance
-c.buy_pct = 80
+c.buy_pct = 100
 // sell with this % of asset balance
-c.sell_pct = 80
+c.sell_pct = 100
 // ms to adjust non-filled order after
 c.order_adjust_time = 30000
 // avoid selling at a loss below this pct
